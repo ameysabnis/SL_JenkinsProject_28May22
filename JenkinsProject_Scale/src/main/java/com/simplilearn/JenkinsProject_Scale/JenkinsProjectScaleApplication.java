a1a0bf34734c3f541765593eea18a1739fae22ec
student <amey.sab@gmail.com>
2022-05-28 06:38:09 +0000

    Add Project Skeleton

---
+++ JenkinsProject_Scale/src/main/java/com/simplilearn/JenkinsProject_Scale/JenkinsProjectScaleApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.JenkinsProject_Scale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsProjectScaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsProjectScaleApplication.class, args);
+	}
+
+}
---
+++ JenkinsProject_Scale/src/test/java/com/simplilearn/JenkinsProject_Scale/JenkinsProjectScaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.JenkinsProject_Scale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsProjectScaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
